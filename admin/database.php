6a06e646357b10d183ab5bb3125f5d3c081ae58b
myclgproject <sandy63631@gmail.com>
2024-03-20 09:45:55 +0530

    addded new folder kisan website and changed path of database

--- admin/database.php
+++ admin/database.php
@@ -1,8 +1,8 @@
 <?php
  session_start();
  $con=mysqli_connect("localhost","root","","kisaansuvidha");
- define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/phpadmindashboard/kisaansuvidha/');
- define('SITE_PATH','http://localhost/phpadmindashboard/kisaansuvidha/');
+ define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/phpadmindashboard/kisan-website/');
+ define('SITE_PATH','http://localhost/phpadmindashboard/kisan-website/');
  
  define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
  define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
