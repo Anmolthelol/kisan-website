0ced33f732712f12091fdba1acb5d55c4633712e
[dudeharsh55] <[patilharshal785@gmail.com]>
2024-03-31 09:04:49 +0530

    forget password

---
+++ forgot_password.php
@@ -0,0 +1,79 @@
+<?php 
+require('top.php');
+if(isset ($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']=='yes'){
+   ?>
+   <script>
+    window.location.href='my_order.php';
+   </script>
+   <?php
+}
+?>
+?>
+<!-- Start Bradcaump area -->
+<div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+    <div class="ht__bradcaump__wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="bradcaump__inner">
+                        <nav class="bradcaump-inner">
+                            <a class="breadcrumb-item" href="index.php">Home</a>
+                            <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                            <span class="breadcrumb-item active">Forgot Password</span>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Bradcaump area -->
+<section class="htc__contact__area ptb--100 bg__white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="contact-form-wrap mt--60">
+                    <div class="col-xs-12">
+                        <div class="contact-title">
+                            <h2 class="title__line--6">Login</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <form id="login-form" method="post">
+                            <div class="single-contact-form">
+                                <div class="contact-box name">
+                                    <input type="text" name="email" id="email" placeholder="Your Email*" style="width:100%">
+                                </div>
+                                <span class="field_error" id="email_error"></span>
+                            </div>
+                            <div class="contact-btn">
+                                <button type="button" class="fv-btn" onclick="forgot_password()">Submit</button>
+                            </div>
+                        </form>
+                        <div class="form-output login_msg">
+                            <p class="form-messege field_error"></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+</section>
+<script>
+    function forgot_password(){
+        var email=jQuery('#email').val();
+        if(email==''){
+            jQuery('#email_error').html('please enter email id');
+        }else{
+            jQuery.ajax({
+                url:'forgot_password_submit.php',
+                type:'post',
+                data:'email'+email,
+                success:function(result){
+                    jQuery('email_error').html(result)
+                }
+            })
+        }
+    }
+</script>
+<?php require('footer.php') ?>
